4e98abe3827f72bd92e43c80b6dba707ffcc0620
GausShaikh <110074462+GausShaikh@users.noreply.github.com>
2022-10-12 20:16:19 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,9 @@
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility = 'visible';
+    document.getElementById('quicksearch').style.opacity='0.7'
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden';
+    document.getElementById('quicksearch').style.opacity='1'
+}
